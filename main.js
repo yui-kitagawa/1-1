3112fb334363d3aa99106f767a7389c199eca466
yui-kitagawa <105733455+yui-kitagawa@users.noreply.github.com>
2022-05-18 21:00:48 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("Hello World!!");
+console.log(65536);
+console.log(10 + 5);
+console.log(10 * 5);
+console.log("HelloWold!!");
+console.log("Hello" + "Wold!!");
+
+//割り算の余りの値を求めたい場合は「％」を使用します
+console.log(10 % 5);
+/*
+上記の場合、10割る２であまりは0
+そのため出力は「0」
+下記の場合、出力は「1」
+*/
+console.log(10 % 3);
+
+//課題
+console.log("10+8");
+console.log(10 + 8);
+console.log("20割る3の余りは" + 20 % 3);
+
+window.alert("Hello World!!");
